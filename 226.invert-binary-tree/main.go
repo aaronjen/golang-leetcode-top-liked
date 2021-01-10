7f08427e8b9b8a2eca99c03b6863e5429a6cefba
Aaron Jen <aaronjen@jubo.health>
2021-01-10 18:16:31 +0800

    commit invert-binary-tree

---
+++ 226.invert-binary-tree/main.go
@@ -0,0 +1,26 @@
+package main
+
+/*
+ * @lc app=leetcode id=226 lang=golang
+ *
+ * [226] Invert Binary Tree
+ */
+
+// TreeNode Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// @lc code=start
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	root.Left, root.Right = invertTree(root.Right), invertTree(root.Left)
+	return root
+}
+
+// @lc code=end
