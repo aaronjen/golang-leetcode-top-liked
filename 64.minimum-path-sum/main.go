0f821b1e0c3051a8f6760183cecca5d4bafe106c
Aaron Jen <aaronjen@jubo.health>
2021-01-31 16:02:26 +0800

    commit [64] Minimum Path Sum

---
+++ 64.minimum-path-sum/main.go
@@ -0,0 +1,41 @@
+package main
+
+/*
+ * @lc app=leetcode id=64 lang=golang
+ *
+ * [64] Minimum Path Sum
+ */
+
+// @lc code=start
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+func minPathSum(grid [][]int) int {
+	m := len(grid)
+	n := len(grid[0])
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+	}
+	dp[0][0] = grid[0][0]
+	for i := 1; i < m; i++ {
+		dp[i][0] = dp[i-1][0] + grid[i][0]
+	}
+	for j := 1; j < n; j++ {
+		dp[0][j] = dp[0][j-1] + grid[0][j]
+	}
+
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+		}
+	}
+
+	return dp[m-1][n-1]
+}
+
+// @lc code=end
